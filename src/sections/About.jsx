7874aa963fdf3b53227db03c82c30bf5db69ad7c
EmaSuriano <emanuel.suriano@gmail.com>
2019-11-13 22:56:45 +0000

    adding about text v1

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -30,15 +30,19 @@ const BioAvatarInner = styled.div`
   width: 300px;
   border-radius: 50%;
   background: rgba(0, 0, 0, 0.25);
-  margin-left: 16px;
   overflow: hidden;
+  margin-left: 16px;
+
+  ${mediaqueries.tablet`
+    margin-left: 0;
+    margin-top: 16px;
+  `}
 `;
 
 const About = () => {
   const { about } = useStaticQuery(siteQuery);
   const author = local.authors(useAuthorsQuery().authors.edges[0]);
 
-  console.log(author.avatar);
   return (
     <Section title="About me">
       <InfoWrapper>
