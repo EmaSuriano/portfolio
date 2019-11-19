fdd9cdb5e59b90e7ee59d0441c53afe530dd0daa
EmaSuriano <emanuel.suriano@gmail.com>
2019-11-19 21:09:55 +0100

    adding logo!

---
+++ src/@narative/gatsby-theme-novela/components/Logo/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import mediaqueries from '@narative/gatsby-theme-novela/src/styles/media';
+
+const Hologram = styled.h1`
+  display: inline-block;
+  padding: 1em 1.7em;
+  color: ${p => p.color};
+  position: relative;
+  transition: translate 250ms ease;
+
+  ${mediaqueries.tablet`
+    
+    font-size: 0.8em;
+  `}
+
+  &:before,
+  &:after {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    border: 2px solid;
+    transition: 0.25s;
+  }
+
+  &:before {
+    transform: translateX(-0.25em) translateY(0.25em);
+  }
+  &:after {
+    transform: translateX(0.25em) translateY(-0.25em);
+  }
+
+  &:hover {
+    &:before,
+    &:after {
+      transform: translateX(0) translateY(0);
+    }
+  }
+`;
+
+const Logo = ({ fill = 'white' }) => <Hologram color={fill}>ES</Hologram>;
+
+export default Logo;
