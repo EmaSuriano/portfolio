c5874977802bd0f8a7e5ff9c310b3f55eab52356
EmaSuriano <emanuel.suriano@gmail.com>
2019-11-20 22:25:54 +0100

    trying new form

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -7,7 +7,38 @@ const About = () => {
 
   return (
     <Section title="Get in Touch">
-      <form name="contact" method="POST" data-netlify="true">
+      <form
+        name="contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <input type="hidden" name="bot-field" />
+
+        <label>
+          Name
+          <input type="text" name="name" id="name" required />
+        </label>
+        <label>
+          Email
+          <input type="text" name="email" id="email" required />
+        </label>
+        <div>
+          <label>
+            Message
+            <textarea name="message" id="message" rows="6" required />
+          </label>
+          <ul className="actions">
+            <li>
+              <input type="submit" value="Send Message" className="special" />
+            </li>
+            <li>
+              <input type="reset" value="Clear" />
+            </li>
+          </ul>
+        </div>
+      </form>
+      {/* <form name="contact" method="POST" data-netlify="true">
         <label>
           Your Name: <input type="text" name="name" />
         </label>
@@ -20,7 +51,7 @@ const About = () => {
           </label>
           <button type="submit">Send</button>
         </div>
-      </form>
+      </form> */}
     </Section>
   );
 };
