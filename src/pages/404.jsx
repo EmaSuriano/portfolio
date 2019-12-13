94e30be9daf14c62f6ee8e8b3f732cc141dc4fe5
EmaSuriano <emanuel.suriano@gmail.com>
2019-12-13 11:22:35 +0100

    move text-align

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -24,6 +24,8 @@ const NotFoundThemed = styled(NotFound)`
 
 const MessageContainer = styled.div`
   margin-top: 80px;
+  text-align: center;
+
   & h1 {
     margin-bottom: 30px;
   }
@@ -49,7 +51,6 @@ const NotFoundPage = () => (
     <Section
       css={css`
         margin-top: 100px;
-        text-align: center;
         z-index: 1;
         position: relative;
       `}
