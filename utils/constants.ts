808d9682339afdcfc757fbf55b8af84a9d0a7315
EmaSuriano <emanuel.suriano@gmail.com>
2020-10-27 00:15:18 +0100

    fixing regex

--- utils/constants.ts
+++ utils/constants.ts
@@ -8,7 +8,7 @@ export const POST_EXTENSION = 'mdx-raw';
 export const POST_SECRET_EXTENSION = 'mdx-secret';
 
 export const POSTS_GLOB_PATTERN_FINAL = `${CONTENT_FOLDER}/posts/**/*.mdx`;
-export const POSTS_GLOB_PATTERN = `${CONTENT_FOLDER}/posts/**/*.[${POST_EXTENSION}|${POST_SECRET_EXTENSION}]`;
+export const POSTS_GLOB_PATTERN = `${CONTENT_FOLDER}/posts/**/*.{${POST_EXTENSION},${POST_SECRET_EXTENSION}}`;
 export const ABOUT_GLOB_PATTERN = `${CONTENT_FOLDER}/about/authors.yml`;
 export const TALKS_GLOB_PATTERN = `${CONTENT_FOLDER}/talks/**/*.yml`;
 export const PROJECTS_GLOB_PATTERN = `${CONTENT_FOLDER}/projects/**/*.yml`;
