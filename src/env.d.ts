0cc3cf52c744ded3a6082d07e77ba8d028912ef6
Emanuel Suriano <emanuel.suriano@revolut.com>
2023-01-18 10:29:03 +0100

    fix build"

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,1 @@
 /// <reference types="@astrojs/image/client" />
-
-declare module 'astro-imagetools/components' {
-  import { ImgConfigOptions } from 'astro-imagetools';
-
-  // real implementation --> node_modules/astro-imagetools/components/Img.astro
-  export function Img(props: ImgConfigOptions): Node;
-}
