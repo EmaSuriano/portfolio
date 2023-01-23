ce5413f002644757497ddc003a58d2993c5e88d8
Emanuel Suriano <emanuel.suriano@revolut.com>
2023-01-23 10:59:25 +0100

    group by year

--- src/helpers.ts
+++ src/helpers.ts
@@ -13,3 +13,19 @@ export const sortPostByDate = (a: Post, b: Post) =>
     parseISO(b.frontmatter.publishedAt),
     parseISO(a.frontmatter.publishedAt),
   );
+
+type Group = { year: number; posts: Post[] };
+
+export const groupPostsByDate = (posts: Post[]): Group[] => {
+  const grouped = posts.reduce((acc, curr) => {
+    const year = parseISO(curr.frontmatter.publishedAt).getFullYear();
+    return { ...acc, [year]: acc[year] ? acc[year].concat(curr) : [curr] };
+  }, {} as Record<string, Post[]>);
+
+  return Object.entries(grouped)
+    .map(([year, posts]) => ({
+      year: Number(year),
+      posts: posts.sort(sortPostByDate),
+    }))
+    .sort((a, b) => b.year - a.year);
+};
