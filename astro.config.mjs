8ec486a422df13f7d800408569748b4d3d8a0a53
Ema Suriano <emanuel.suriano@gmail.com>
2024-03-22 12:39:35 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,6 +20,6 @@ export default defineConfig({
     remarkPlugins,
   },
   redirects: {
-    '/resume': 'https://resume.emasuriano.com'
-  }
+    '/resume': 'https://resume.emasuriano.com',
+  '/avatar': "https://secure.gravatar.com/avatar/de8225213d7b8bc59895625735574f7d?s=2048",  }
 });
