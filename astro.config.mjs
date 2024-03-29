31d756db5fdc3da5df840d56673dd219d45b3c39
Emanuel Suriano <emanuel.suriano@gmail.com>
2024-03-29 13:58:26 +0800

    update avatar image with cdn one

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,5 +21,5 @@ export default defineConfig({
   },
   redirects: {
     '/resume': 'https://resume.emasuriano.com',
-  '/avatar': "https://secure.gravatar.com/avatar/de8225213d7b8bc59895625735574f7d?s=2048",  }
+    '/avatar': "https://cdn.statically.io/gh/emasuriano/static/main/assets/avatar.jpeg",  }
 });
