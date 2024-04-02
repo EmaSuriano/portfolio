024e5350a13a035f49a41b0cce58b00a11394c3d
Emanuel Suriano <emanuel.suriano@gmail.com>
2024-04-02 10:08:43 +0800

    bump astro deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import sitemap from '@astrojs/sitemap';
 import { astroImageTools } from 'astro-imagetools';
 import mdx from '@astrojs/mdx';
 import remarkPlugins from './remark-plugins';
+import icon from "astro-icon";
+
 
 export default defineConfig({
   site: 'https://emasuriano.com/',
@@ -13,6 +15,7 @@ export default defineConfig({
     sitemap(),
     astroImageTools,
     compress(),
+    icon(),
     mdx({ remarkPlugins, syntaxHighlight: 'prism' }),
   ],
   markdown: {
