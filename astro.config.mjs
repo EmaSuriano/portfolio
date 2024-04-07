99ad53ec7e48c7be0ce6cb30fd57b055a958155d
Ema Suriano <emanuel.suriano@gmail.com>
2024-04-07 11:53:14 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://emasuriano.com/",
+  prefetch: {
+    defaultStrategy: 'viewport'
+  }
   integrations: [
     tailwind(),
     sitemap(),
