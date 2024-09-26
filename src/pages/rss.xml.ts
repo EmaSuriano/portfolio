eef182b033ac74d6bf9c3b4c106afe7ee957a8d5
Emanuel Suriano <emanuel.suriano@revolut.com>
2024-09-26 16:55:19 +0200

    add content to rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,4 +1,7 @@
 import rss from "@astrojs/rss";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 import { getPostLink, type Summary } from "helpers";
 import { GET as getSummary } from "./api/summary.ts";
@@ -17,6 +20,9 @@ export async function GET() {
         pubDate: post.data.publishedAt,
         description: post.data.summary,
         author: summary.name,
+        content: sanitizeHtml(parser.render(post.body), {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+        }),
       };
     }),
   });
